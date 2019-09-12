02602089603090dcb71eab863f3cc1b5339beae2
Mkawera1 <kaweramk@gmail.com>
2019-09-12 11:41:58 -0400

    Intermediate Eliza

--- src/Eliza.java
+++ src/Eliza.java
@@ -1,24 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Eliza {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
-        boolean toQuit = true;
-String elizaSays = "";
+        String elizaSays = "";
+        ArrayList<String>listStrings = new ArrayList<>();
+
+        HashMap<String,String> replacements = new HashMap<>();
+        replacements.put("i", "you");
+        replacements.put("me", "you");
+        replacements.put("my", "your");
+        replacements.put("am", "are");
+
         System.out.println("Good day. What is your problem? ");
 
         do{
             System.out.print("Enter your response here or Q to quit: ");
             input = sc.nextLine();
 
-            if (input.equalsIgnoreCase("Q")) {
-                toQuit = false;
+            if (input.equalsIgnoreCase("Q") ||
+                input.equalsIgnoreCase("I am feeling great")) {
+                break;
             }
 
-            elizaSays = input;
-            System.out.println(elizaSays);
+            for(String s: input.split(" ")){
+                listStrings.add(s);
+            }
+            //System.out.println(listStrings.toString());
 
-        } while(toQuit);
+            for(int i=0; i< listStrings.size(); i++){
+                String tempIndex = listStrings.get(i);
+//
+                if(tempIndex.equalsIgnoreCase("i")){
+                    tempIndex = replacements.get("i");
+                }else if(tempIndex.equalsIgnoreCase("me")){
+                    tempIndex = replacements.get("me");
+                }
+                else if(tempIndex.equalsIgnoreCase("my")) {
+                    tempIndex = replacements.get("my");
+                }
+                else if(tempIndex.equalsIgnoreCase("am")) {
+                    tempIndex = replacements.get("am");
+                }
+                listStrings.set(i,tempIndex);
+            }
+            //System.out.println(listStrings.toString()); //comment out later
+
+            String tempOutput = "";
+            for(String s: listStrings){
+                tempOutput += s + " ";
+            }
+            tempOutput.trim();
+            elizaSays = tempOutput;
+            System.out.println(elizaSays);
+            listStrings = new ArrayList<>();
+        }while(true);
     }
 }
